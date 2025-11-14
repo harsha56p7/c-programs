95c0fd50c08de3915530ea4beeb3dc9d69ff9879
harsha56p7 <56p7saharsha@gmail.com>
2025-11-14 13:07:08 +0530

    Add files via upload

---
+++ sumofnaturalno.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter value of n");
+	scanf("%d",&n);
+	n=n*(n+1)/2;
+	printf("sum of n natural number is %d",n);
+}
---
+++ velocity.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int v,u,a,t;
+	printf("enter values of u,a,t");
+    scanf("%d%d%d",&u,&a,&t);
+	v=u+a*t;
+	printf("final velocity id %d",v);
+}
